17ee46107fc03302f559afe0170d8bd27aadd8e0
Javier Steven Pérez Fernández <perez1932@hotmail.es>
2022-02-17 20:14:49 -0500

    Desestructuración de objetos

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,67 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = (props) => {
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  )
-}
+const Hello = ({name, age}) => {
 
-const Part = (props) => {
+  const bornYear = () => new Date().getFullYear() - age;
+  
   return (
-    <>
+    <div>
       <p>
-        {props.part} {props.exercises}
+        Hello {name}, you are {age} years old
       </p>
-    </>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    </>
+      <p>
+        So you were probably born in {bornYear()}
+      </p>
+    </div>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const name = 'Peter'
+  const age = 10
 
-  return(
+  return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div >
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
   )
 }
 
